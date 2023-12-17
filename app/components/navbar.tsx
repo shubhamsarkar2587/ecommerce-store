548eb8fafc4681678da4496755fd334e5a826c88
Shubham <shubhamsarkar9654@gmail.com>
2023-12-17 19:11:30 +0530

    created navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,23 @@
-const Navbar = () => {
+import Link from "next/link";
+import Container from "./ui/container";
+import MainNav from "./main-nav";
+import getCategories from "@/actions/get-catgories";
+import NavbarActions from "./navbar-actions";
+
+const Navbar = async () => {
+  const categories = await getCategories();
   return (
-    <div>Navbar</div>
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+        <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+          <p className="font-bold text-xl">STORE</p>
+        </Link>
+        <MainNav data={categories} />
+        <NavbarActions />
+        </div>
+      </Container>
+    </div>
   )
 }
 
