fd51ea465a5e70d4bf3ee4d2ad20a9d258fcc43e
Shubham <shubhamsarkar9654@gmail.com>
2023-12-18 23:51:16 +0530

    - created product list flow

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,16 +1,22 @@
 import getBillboard from "@/actions/get-billboard";
 import Billboard from "../components/billboard";
 import Container from "../components/ui/container";
+import getProducts from "@/actions/get-products";
+import ProductList from "../components/product-list";
 
 export const revalidate = 0;
 
 const Homepage = async () => {
+  const products = await getProducts({ isFeatured: true })
   const billboard = await getBillboard('6cf16b3c-29d7-4c20-851c-effb3ff35297')
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   )
