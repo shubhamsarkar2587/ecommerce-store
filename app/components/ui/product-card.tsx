6fce4c5469ac1f7528ec674fe5a3a625c43f145f
Shubham <shubhamsarkar9654@gmail.com>
2023-12-22 00:06:26 +0530

    order flow created

--- app/components/ui/product-card.tsx
+++ app/components/ui/product-card.tsx
@@ -9,6 +9,7 @@ import { Product } from "@/type";
 import IconButton from "./icon-button";
 import Currency from "./currency";
 import usePreviewModal from "@/hooks/user-preview-modal";
+import useCart from "@/hooks/user-cart";
 
 interface ProductCardProps {
   data: Product;
@@ -17,6 +18,7 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   const router = useRouter();
   const previewModal = usePreviewModal()
+  const cart = useCart()
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
@@ -26,6 +28,12 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
     event.stopPropagation()
 
     previewModal.onOpen(data)
+  }
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation()
+
+    cart.addItem(data)
   }
 
   return (
@@ -48,7 +56,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}}
+              onClick={onAddToCart}
               icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
